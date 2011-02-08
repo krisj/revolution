f6ef02523be196eb6208e58c50bde16afd2c1089
Krisjanis Berzins <krisjanis.b@gmail.com>
2011-02-08 16:28:34 +0000

    Added initial Latvian language pack
    Signed-off-by: Krisjanis Berzins <krisjanis.b@gmail.com>

---
+++ core/lexicon/lv/system_events.inc.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * System Events Latvian lexicon topic
+ *
+ * @language lv
+ * @package modx
+ * @subpackage lexicon
+ */
+$_lang['clear'] = 'Clear';
+$_lang['error_log'] = 'Error Log';
+$_lang['error_log_desc'] = 'Here is the error log for MODx Revolution:';
+$_lang['system_events'] = 'System Events';
+$_lang['priority'] = 'Priority';
